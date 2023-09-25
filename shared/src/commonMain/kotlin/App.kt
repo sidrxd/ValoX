7c139a09a31e97aabd01b0d28f5a6bcdd156c538
siddharth shakya <sid95.private@gmail.com>
2023-09-25 12:28:09 +0530

    Added logo at launch

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,34 +1,39 @@
-import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.Navigator
 import connection.ApiHelper
 import connection.ApiResult
 import connection.MainRepository
 import connection.ValorantApiResponse
 import kotlinx.coroutines.flow.MutableStateFlow
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
 import screen.MainScreen
 import ui.theme.Color
+import ui.theme.Dimens
 import utils.Coroutines
-import utils.Utils
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
     val agents = MutableStateFlow(ArrayList<ValorantApiResponse.Data>())
     Coroutines.io {
-        val result = MainRepository(ApiHelper.client).getEntries()
-        when (result) {
+        when (val result = MainRepository(ApiHelper.client).getEntries()) {
             is ApiResult.APIError -> {
 
             }
@@ -49,15 +54,21 @@ fun App() {
                 modifier = Modifier.fillMaxSize().background(Color.colorBackground),
                 contentAlignment = Alignment.Center
             ) {
-                Column {
-                    Text(
-                        text = "ValoX",
-                        fontSize = 48.sp,
-                        fontWeight = FontWeight(800),
-                        color = Utils.hexToColor("FF4654")
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(
+                        painter = painterResource("logo.png"),
+                        null,
+                        modifier = Modifier.padding(
+                            16.dp,
+                            16.dp
+                        ).height(Dimens.DP_120).width(Dimens.DP_120).clip(
+                            RoundedCornerShape(Dimens.DP_16)
+                        )
                     )
-                    LinearProgressIndicator(color = Color.titleText)
-
+                    LinearProgressIndicator(
+                        color = Color.titleText,
+                        modifier = Modifier.width(Dimens.DP_120).clip(RoundedCornerShape(Dimens.DP_16))
+                    )
                 }
             }
         } else {
